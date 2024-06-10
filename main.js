643e58f91185a033f935821bdc45667465aa7700
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-10 11:35:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let ism = prompt ('Ismingizni kiriting  ')
+let harf = prompt('Harf kirining')
+
+
+if(ism.includes(harf)){
+    alert('ismda bundan harf bor')
+}else{
+    alert('ismda bundan harf yoq')
+}
